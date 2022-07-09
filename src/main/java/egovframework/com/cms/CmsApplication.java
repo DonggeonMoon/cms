ea30dee83ffadc1fe2839265473682593c2eef8d
DonggeonMoon <nullsafer@naver.com>
2022-07-09 20:03:40 +0900

    feat: 기본 Spring 애플리케이션 설정

--- src/main/java/egovframework/com/cms/CmsApplication.java
+++ src/main/java/egovframework/com/cms/CmsApplication.java
@@ -1,13 +1,13 @@
 package egovframework.com.cms;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class CmsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CmsApplication.class, args);
     }
-
 }
