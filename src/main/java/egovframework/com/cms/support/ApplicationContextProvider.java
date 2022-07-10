c57ea4e51440fa780a137d4b32b3d2367b00948f
DonggeonMoon <nullsafer@naver.com>
2022-07-10 22:36:48 +0900

    feat: ApplicationContextProvider, ServerMessage 작성

---
+++ src/main/java/egovframework/com/cms/support/ApplicationContextProvider.java
@@ -0,0 +1,18 @@
+package egovframework.com.cms.support;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class ApplicationContextProvider implements ApplicationContextAware {
+    private static ApplicationContext applicationContext;
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+    }
+}
