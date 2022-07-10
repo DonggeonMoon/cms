2a7f33f3ee9f315e352440318124f9d95b37ae37
DonggeonMoon <nullsafer@naver.com>
2022-07-10 19:35:35 +0900

    feat: interceptor 설정

---
+++ src/main/java/egovframework/com/cms/support/interceptor/SiteInterceptor.java
@@ -0,0 +1,10 @@
+package egovframework.com.cms.support.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@Slf4j
+public class SiteInterceptor implements HandlerInterceptor {
+}
---
+++ src/main/java/egovframework/com/cms/support/interceptor/UserInterceptor.java
@@ -0,0 +1,4 @@
+package egovframework.com.cms.support.interceptor;
+
+public class UserInterceptor {
+}
