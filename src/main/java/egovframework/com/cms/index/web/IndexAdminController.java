a1c385b32bbc77d7cb3605af1aac654027bef329
DonggeonMoon <nullsafer@naver.com>
2022-07-10 15:49:20 +0900

    feat: index controller 작성

--- src/main/java/egovframework/com/cms/index/web/IndexAdminController.java
+++ src/main/java/egovframework/com/cms/index/web/IndexAdminController.java
@@ -1,25 +1,53 @@
 package egovframework.com.cms.index.web;
 
+import egovframework.com.cms.site.model.Site;
 import egovframework.com.cms.support.Constant;
+import egovframework.com.cms.support.annotation.CurrentSite;
+import egovframework.com.cms.support.util.Utils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class IndexAdminController {
-    @GetMapping("/")
-    public String indexRedirectGet() {
-        return "redirect:/site/main/home";
+    @GetMapping("/admin")
+    public String indexAdminGet() {
+        return "redirect:/site/main/admin/login.do";
     }
 
-    @GetMapping(Constant.APP_PATH + Constant.SITE_ID_PATH + "/home")
-    public String indexGet(HttpServletRequest request) {
-        String theme = (String) request.getAttribute("theme");
-        return "theme/" + theme + "/index/home";
+    @RequestMapping(value = Constant.APP_PATH + Constant.SITE_ID_PATH + Constant.ADMIN_PATH + ".do")
+    public String adminShortGet(@CurrentSite Site currentSite) {
+        return Utils.getAdminPath("redirect:", currentSite) + "/login.do";
+    }
+
+    @GetMapping(Constant.APP_PATH + Constant.SITE_ID_PATH + Constant.ADMIN_PATH + "/frame.do")
+    public String frameGet() {
+        return "admin/frame/frame";
+    }
+
+    @GetMapping(Constant.APP_PATH + Constant.SITE_ID_PATH + Constant.ADMIN_PATH + "/frame/top.do")
+    public String frameTopGet() {
+        return "admin/frame/top";
+    }
+
+    @GetMapping(Constant.APP_PATH + Constant.SITE_ID_PATH + Constant.ADMIN_PATH + "/frame/bottom.do")
+    public String frameBottomGet() {
+        return "admin/frame/bottom";
+    }
+
+    @GetMapping(Constant.APP_PATH + Constant.SITE_ID_PATH + Constant.ADMIN_PATH + "/frame/left.do")
+    public String frameleftGet() {
+        return "admin/frame/left";
+    }
+
+    @GetMapping(Constant.APP_PATH + Constant.SITE_ID_PATH + Constant.ADMIN_PATH + "/refreshDummy.do")
+    public void adminRefreshDummyGet(HttpServletResponse response) {
+        response.setStatus(HttpServletResponse.SC_NO_CONTENT);
     }
 }
