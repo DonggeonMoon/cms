003a69c25d64d1fe7df1ea1beddcb7d2a1c3b7b8
DonggeonMoon <nullsafer@naver.com>
2022-07-10 22:24:19 +0900

    feat: 인터셉터들 등록

--- src/main/java/egovframework/com/cms/support/configuration/MvcConfiguration.java
+++ src/main/java/egovframework/com/cms/support/configuration/MvcConfiguration.java
@@ -1,9 +1,42 @@
 package egovframework.com.cms.support.configuration;
 
+import egovframework.com.cms.support.interceptor.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 @RequiredArgsConstructor
-public class MvcConfiguration {
+public class MvcConfiguration implements WebMvcConfigurer {
+    private final SiteInterceptor siteInterceptor;
+    private final SSLInterceptor sslInterceptor;
+    private final SecurityInterceptor securityInterceptor;
+    private final DesignInterceptor designInterceptor;
+    private final UserInterceptor userInterceptor;
+    private final BoardInterceptor boardInterceptor;
+    private final AdminInterceptor adminInterceptor;
+    private final StatisticsInterceptor statisticsInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(siteInterceptor).addPathPatterns("/site/**");
+        registry.addInterceptor(sslInterceptor).addPathPatterns("/site/**");
+        registry.addInterceptor(securityInterceptor).addPathPatterns("/site/**");
+        registry.addInterceptor(designInterceptor).addPathPatterns("/site/**");
+        registry.addInterceptor(localeChangeInterceptor()).addPathPatterns("/site/**");
+        registry.addInterceptor(userInterceptor).addPathPatterns("/site/**");
+        registry.addInterceptor(boardInterceptor).addPathPatterns("/site/**/board/**");
+        registry.addInterceptor(adminInterceptor).addPathPatterns("/site/**/admin/**/*.do");
+        registry.addInterceptor(securityInterceptor).addPathPatterns("/site/**");
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        return new LocaleChangeInterceptor();
+    }
+
+
 }
