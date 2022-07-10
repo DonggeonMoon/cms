ca402ca134f19db35416edf126dc4fe178d05db1
DonggeonMoon <nullsafer@naver.com>
2022-07-10 16:22:30 +0900

    feat: annotation 작성

---
+++ src/main/java/egovframework/com/cms/support/annotation/CurrentMenu.java
@@ -0,0 +1,9 @@
+package egovframework.com.cms.support.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface CurrentMenu {
+}
