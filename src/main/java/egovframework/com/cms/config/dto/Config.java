27db6109603bd05789daf18cfd1b05a0c4f2a352
DonggeonMoon <nullsafer@naver.com>
2022-07-26 19:37:20 +0900

    feat: Config.java 메서드 작성

--- src/main/java/egovframework/com/cms/config/dto/Config.java
+++ src/main/java/egovframework/com/cms/config/dto/Config.java
@@ -4,6 +4,7 @@ import egovframework.com.cms.config.model.ConfigOption;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
@@ -14,11 +15,10 @@ import java.util.Map;
 @Setter
 @Slf4j
 public class Config {
-
     private String confId;
     private String sitePrefix;
     private List<ConfigOption> options;
-    private Map<String, String> optionMap = new HashMap<String, String>();//jsp 뷰단에서 꺼내쓰려고 만든 변수임. 자바단에서는 쓸일 없음.
+    private Map<String, String> optionMap = new HashMap<String, String>();
     private MultipartFile watermarkImage;
 
     public Config(List<ConfigOption> options) {
@@ -27,15 +27,35 @@ public class Config {
 
     public String getOption(String optKey) {
         for (ConfigOption configOption : options) {
-            if (optKey.equals(configOption.getOptKey())) {
-                if (configOption.getOptValue() == null) {
-                    log.info("Config option value for option key [{}] is null, check option table.", optKey);
-                    return "";
-                } else {
-                    return configOption.getOptValue();
-                }
+            if (optKey.equals(configOption.getOptKey()) && configOption.getOptValue() == null) {
+                log.info("Config option value for option key [{}] is null. Check option table.", optKey);
+                return "";
+            }
+            if (optKey.equals(configOption.getOptKey()) && configOption.getOptValue() != null) {
+                return configOption.getOptValue();
             }
         }
         return null;
+    }
+
+    public Map<String, String> getOptionMap() {
+        if (!this.optionMap.isEmpty()) {
+            return optionMap;
+        }
+        for (ConfigOption configOption : options) {
+            if (StringUtils.isNotBlank(configOption.getOptKey())) {
+                optionMap.put(configOption.getOptKey(), configOption.getOptValue());
+            }
+        }
+        return optionMap;
+    }
+
+    public void updateOption(String key, String value) {
+        for (ConfigOption configOption : options) {
+            if (configOption.getOptKey().equals(key)) {
+                configOption.toResponseDto().setOptValue(value);
+                break;
+            }
+        }
     }
 }
