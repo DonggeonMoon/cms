09869acb20fd004641742c82f0963dcd59050b23
Donggeon Moon <nullsafer@naver.com>
2024-03-06 12:55:32 +0900

    feat: 게시판 기능

---
+++ src/main/java/com/dgmoonlabs/cms/domain/board/entity/BoardConfig.java
@@ -0,0 +1,17 @@
+package com.dgmoonlabs.cms.domain.board.entity;
+
+import com.dgmoonlabs.cms.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class BoardConfig extends BaseEntity {
+    @Id
+    private Long id;
+
+
+    private String title;
+
+
+}
+
